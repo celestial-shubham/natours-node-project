f07c9a66bcacfde5417e65fabfd5c2f8c4318820
shubham verma <shubham.verma@zupee.in>
2023-07-16 20:25:47 +0530

    removed logs

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -5,10 +5,8 @@ export const bookTour = async tourID => {
   const stripe = Stripe('pk_test_51NOFqlSBmcnW8TJY7aG4VYfZJf9hENTn0Udu80wE67wKIZOC0MhH4YgUOBSXYkyY6v15HdmymuUEMnfAxgfwWsua00EYTyOShC');
   // 1 GET CheckOut session from API
   try {
-    console.log("value of tour ---->" , tourID)
     // 1. Get checkout session from the API
     const session = await axios(`/api/v1/booking/checkout-session/${tourID}`);
-    console.log(session);
 
     // 2. Create checkout form + charge credit card
     await stripe.redirectToCheckout({
